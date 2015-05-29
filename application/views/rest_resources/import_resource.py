5a9467351a2ab94643b502b7dcf16098286a2bc0
Xyder <xydr2311@gmail.com>
2015-05-29 10:17:11 +0300

    - added a mock get for ImportedResource

--- application/views/rest_resources/import_resource.py
+++ application/views/rest_resources/import_resource.py
@@ -1,4 +1,4 @@
-from flask.ext.restful import Resource
+from flask.ext.restful import Resource, abort
 from application.utils import Authentication
 
 
@@ -9,4 +9,21 @@ class ImportedResource(Resource):
 
     @Authentication.login_required
     def get(self, file_name='', item_id='', attr=''):
-        pass
+        if not file_name:
+            # file list is requested
+                return {'files': 'files'}
+
+        if not item_id:
+            # root items are requested
+                return {'root_items': 'root items'}
+
+        if not attr:
+            # item object is requested
+                return {'item': 'item'}
+
+        if attr == 'children':
+            # item children are requested
+                return {'children': 'children'}
+
+        # bad request
+        abort(400, message='Request could not be satisfied.')
